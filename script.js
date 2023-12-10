62c405d645410b62b337661df02877c61bbdecb9
ckuzmick <conrad.kuzmick@icloud.com>
2023-12-09 23:32:00 -0500

    stuff

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var score = 1;
+var score = 0;
 var scoreDiv = document.getElementById("score");
+var clickIncrement = 1;
+var autoIncrement = 0;
 
 function clicker() {
-    score++;
-    scoreDiv.innerHTML = score;
+    score += clickIncrement;
     console.log(score)
 }
+
+function auto() {
+    score += autoIncrement;
+    console.log(score)
+}
+
+function upgradeClick() {
+    clickIncrement += 2;
+}
+
+function upgradeAuto() {
+    autoIncrement += 0.01;
+}
+
+function update() {
+    scoreDiv.innerHTML = Math.floor(score);
+}
+
+setInterval(update, 10);
+setInterval(auto, 10);
